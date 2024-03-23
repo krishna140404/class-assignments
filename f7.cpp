ecb312215ac9da3e2a23850babdb475657997728
krishna kachhiapatel <krishnanachhiapatel14@gmail.com>
2024-03-22 23:26:46 -0400

    second commit, the binary file is created and the logic for it works perfectly.

--- f7.cpp
+++ f7.cpp
@@ -1,10 +1,4 @@
-/*
-* FILE : f7.cpp
-* PROJECT : focus Assignment 7
-* PROGRAMMER : krishna kachhiapatel
-* FIRST VERSION : 2024-03-19
-* DESCRIPTION : Write a program to give you practice at creating text and binary files.
-*/
+
 #include <stdio.h>
 
 #pragma warning(disable:4996) 
@@ -25,6 +19,27 @@ int main(void)
     fprintf(textFile, "This is line 1.\nThis is line 2.\n");
     fclose(textFile);
 
+    // creating and opening a binary file 
+    FILE* binaryFile = fopen("myEvenDataList.data", "wb");
+    if (!binaryFile)
+    {
+        printf("Error opening binary file\n");
+        return 1;
+    }
+    //this for is made to enter the binary number in binary file from the array. 
+    for (int i = 0; i < sizeof(kBinaryData) / sizeof(kBinaryData[0]); ++i)
+    {
+        if (kBinaryData[i] % 2 == 0)
+        {
+            if (fwrite(&kBinaryData[i], sizeof(short), 1, binaryFile) != 1)
+            {
+                printf("Error writing to binary file\n");
+                fclose(binaryFile);
+                return 1;
+            }
+        }
+    }
+    fclose(binaryFile);
   
 
     return 0;
