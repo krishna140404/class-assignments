e5b5bf20e2da23d1fc5e5e9cf8110b37706f987f
krishna kachhiapatel <krishnanachhiapatel14@gmail.com>
2024-03-24 18:28:39 -0400

    first commit, creat structure and passing the argument into the structure.

--- f8.cpp
+++ f8.cpp
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#pragma warning(disable:4996) 
 
-struct MyData {
-	int howMany;
-	char theText[21];
-	char directoryPath[21];
-	char filename[21];
+#define MAX_LENGTH 21
+
+#pragma warning (disable:4996)
+
+struct MyData
+{
+    int howMany;
+    char theText[MAX_LENGTH];
+    char directoryPath[MAX_LENGTH];
+    char filename[MAX_LENGTH];
+
 };
-int main(int argc, char** argv) {
-	if (argc != 5) {
-		printf("invalid amount of arguments");
-		return 0;
-	}
-	struct MyData myArgs;
-	myArgs.howMany = atoi(argv[1]);
-	strncpy(myArgs.theText, argv[2], sizeof(myArgs.theText) - 1);
-	strncpy(myArgs.directoryPath, argv[3], sizeof(myArgs.directoryPath) - 1);
-	strncpy(myArgs.filename, argv[4], sizeof(myArgs.filename) - 1);
 
-	FILE* file;
-	file = fopen(myArgs.filename, "w");
-	if (!file) {
-		printf("unable to create a the file");
-		return 0;
-	}
-	for (int i = 0; i < myArgs.howMany; i++) {
-		fprintf(file, "%s\n", myArgs.theText);
-	}
-	return 0;
+int main(int argc, char* argv[])
+{
+
+    if (argc != 5)
+    {
+        fprintf(stderr, "Error: Incorrect number of arguments. Please provide exactly four arguments.\n");
+        return 1;
+    }
+
+
+    // Parse arguments into MyData struct
+    struct MyData myArgs;
+    myArgs.howMany = atoi(argv[1]);
+    strncpy(myArgs.theText, argv[2], MAX_LENGTH - 1);
+    myArgs.theText[MAX_LENGTH - 1] = '\0';
+    strncpy(myArgs.directoryPath, argv[3], MAX_LENGTH - 1);
+    myArgs.directoryPath[MAX_LENGTH - 1] = '\0';
+    strncpy(myArgs.filename, argv[4], MAX_LENGTH - 1);
+    myArgs.filename[MAX_LENGTH - 1] = '\0';
+
+
+
+
+    return 0;
 }
-
